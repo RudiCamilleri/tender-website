bfccf28ebfbcc3291de17deb843860ffb3bcc3c8
Daniel Anthony Cilia <daniel-anthony.a.cilia@gov.mt>
2018-12-05 18:14:51 +0100

    Created ASP.NET Project 'TenderWebsite'. Started working on login page using Andrea's HTML code.

---
+++ TenderWebsite/TenderWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TenderWebsite.Startup))]
+namespace TenderWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
