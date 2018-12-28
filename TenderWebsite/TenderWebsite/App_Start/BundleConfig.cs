47e204836ee9e1e9fa6c4781b2ac2905482632e8
Andrea Abela <abela108@gmail.com>
2018-12-28 09:54:45 +0100

    Created base layout with navbar and sticky footer

--- TenderWebsite/TenderWebsite/App_Start/BundleConfig.cs
+++ TenderWebsite/TenderWebsite/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace TenderWebsite
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
