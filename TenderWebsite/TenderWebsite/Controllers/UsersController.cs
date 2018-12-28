a883edf5e1a8d3df368b7a11dab6c8a93d2bb849
Andrea Abela <abela108@gmail.com>
2018-12-28 11:24:07 +0100

    Added home view

--- TenderWebsite/TenderWebsite/Controllers/UsersController.cs
+++ TenderWebsite/TenderWebsite/Controllers/UsersController.cs
@@ -8,7 +8,23 @@ namespace TenderWebsite.Controllers
 {
     public class UsersController : Controller
     {
+        [HttpGet]
         public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            //login logic here
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Home()
         {
             return View();
         }
