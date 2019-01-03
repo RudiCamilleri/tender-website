428c90da6b11b9e5e81dc0c5c5808738602ea6a3
Andrea Abela <abela108@gmail.com>
2019-01-03 08:14:07 +0100

    Added base controllers

---
+++ TenderWebsite/TenderWebsite/Controllers/DeliveriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TenderWebsite.Controllers
+{
+    public class DeliveriesController : Controller
+    {
+        // GET: Deliveries
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TenderWebsite/TenderWebsite/Controllers/ExtensionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TenderWebsite.Controllers
+{
+    public class ExtensionsController : Controller
+    {
+        // GET: Extensions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
