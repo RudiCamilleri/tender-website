e7e8209eaa0dcee450a5d9f9510f6364e54194c0
Daniel Anthony Cilia <daniel-anthony.a.cilia@gov.mt>
2019-02-22 11:37:27 +0100

    Added API Project.

---
+++ tenderwebsiteapi/tenderwebsiteapi/Controllers/ContractsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using tenderwebsiteapi.Models;
+
+namespace tenderwebsiteapi.Controllers
+{
+    public class ContractsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Contracts
+        public IQueryable<Contract> GetContracts()
+        {
+            return db.Contracts;
+        }
+
+        // GET: api/Contracts/5
+        [ResponseType(typeof(Contract))]
+        public async Task<IHttpActionResult> GetContract(string id)
+        {
+            Contract contract = await db.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contract);
+        }
+
+        // PUT: api/Contracts/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutContract(string id, Contract contract)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != contract.Address)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(contract).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContractExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Contracts
+        [ResponseType(typeof(Contract))]
+        public async Task<IHttpActionResult> PostContract(Contract contract)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Contracts.Add(contract);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ContractExists(contract.Address))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = contract.Address }, contract);
+        }
+
+        // DELETE: api/Contracts/5
+        [ResponseType(typeof(Contract))]
+        public async Task<IHttpActionResult> DeleteContract(string id)
+        {
+            Contract contract = await db.Contracts.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            db.Contracts.Remove(contract);
+            await db.SaveChangesAsync();
+
+            return Ok(contract);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContractExists(string id)
+        {
+            return db.Contracts.Count(e => e.Address == id) > 0;
+        }
+    }
+}
